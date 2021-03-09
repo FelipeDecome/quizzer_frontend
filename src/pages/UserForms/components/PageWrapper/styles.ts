56bedc929f84509712cead4055141152b0f71263
Felipe Decome <felipe.decome@hotmail.com>
2021-03-09 18:17:28 -0300

    fix: layout breakpoints problems fixed

--- src/pages/UserForms/components/PageWrapper/styles.ts
+++ src/pages/UserForms/components/PageWrapper/styles.ts
@@ -1,32 +1,53 @@
 import styled from 'styled-components';
 import {
   styleAbove,
-  styleBeetween,
+  styleBetween,
+  styleBelow,
 } from '../../../../shared/styles/mixins/breakpoints';
 
 const Container = styled.div`
   display: flex;
-  justify-content: center;
   align-items: center;
 
   height: 100%;
   width: 100%;
 
-  ${styleBeetween('SM', 'MD')} {
+  ${styleBelow('LG')} {
+    justify-content: center;
+  }
+
+  ${styleBetween('SM', 'LG')} {
     position: relative;
+  }
+
+  ${styleAbove('LG')} {
+    margin: 0 auto;
+  }
+
+  ${styleAbove('XL')} {
+    max-width: 1120px;
+    gap: 16.8rem;
   }
 `;
 
-const Content = styled.div``;
+const Content = styled.div`
+  ${styleBetween('LG', 'XL')} {
+    padding: 12rem;
+  }
+`;
 
 const Complement = styled.div`
-  display: none;
-
-  ${styleAbove('SM')} {
-    display: initial;
+  img {
+    height: 100%;
+    object-fit: cover;
+    object-position: bottom;
   }
 
-  ${styleBeetween('SM', 'MD')} {
+  ${styleBelow('SM')} {
+    display: none;
+  }
+
+  ${styleBetween('SM', 'LG')} {
     position: absolute;
     top: 0;
     height: 40%;
@@ -34,10 +55,11 @@ const Complement = styled.div`
     z-index: -1;
     img {
       width: 100vw;
-      height: 100%;
-      object-fit: cover;
-      object-position: bottom;
     }
+  }
+
+  ${styleAbove('LG')} {
+    height: 100vh;
   }
 `;
 
